32f4faae0dbc19b560d56d310b4612fee1f6ff1d
David Rubino <david.m.rubino@gmail.com>
2015-09-12 17:50:40 -0500

    report

--- footer.php
+++ footer.php
@@ -2,7 +2,15 @@
 echo '
 <nav class="navbar navbar-inverse navbar-fixed-bottom">
 	<div class="container-fluid">
-		<div id="navbar" class="navbar-collapse collapse">
+	<div class="navbar-header">
+			<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar-bottom" aria-expanded="false" aria-controls="navbar">
+				<span class="sr-only">Toggle navigation</span>
+				<span class="icon-bar"></span>
+				<span class="icon-bar"></span>
+				<span class="icon-bar"></span>
+			</button>
+		</div>
+		<div id="navbar-bottom" class="navbar-collapse collapse" role="navigation">
 			<ul class="nav navbar-nav">
 				<li>
 					<a href="#">About</a>
